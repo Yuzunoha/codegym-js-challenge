00d09ac06cdedd4f2337b9985002a0971d0d82b9
EC2 Default User <ec2-user@ip-172-16-2-13.ap-northeast-1.compute.internal>
2022-08-04 08:00:41 +0000

    選択された都市名をJSで取得できた

--- js/weather-api/main.js
+++ js/weather-api/main.js
@@ -1,3 +1,13 @@
 const p = console.log;
 
-p('main.jsです');
+const select_city = document.getElementById('select_city');
+select_city.addEventListener('change', (event) => {
+   p('変更された', event.currentTarget.value);
+});
+
+/*
+https://api.openweathermap.org/data/2.5/weather?q=London&appid=4b5774e9f3d2a07b84f0f2f88e486224
+
+let text = document.getElementById('nametext');
+text.addEventListener('change', inputChange);
+*/
