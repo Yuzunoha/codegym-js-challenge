a504f7c8a1228528b5c825fca548fc0365389645
EC2 Default User <ec2-user@ip-172-16-2-13.ap-northeast-1.compute.internal>
2022-08-04 09:01:29 +0000

    これでいいや完成

--- js/weather-api/main.js
+++ js/weather-api/main.js
@@ -2,24 +2,8 @@ const main = () => {
   const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
   const APPID = '4b5774e9f3d2a07b84f0f2f88e486224';
 
-  const select_city = document.getElementById('select_city');
-  const p_city = document.getElementById('p_city');
-  const p_temp = document.getElementById('p_temp');
-  const p_humidity = document.getElementById('p_humidity');
-  const p_pressure = document.getElementById('p_pressure');
-  const p_description = document.getElementById('p_description');
-
   // url取得関数
   const urlBuilder = (city) => `${baseURL}?lang=ja&units=metric&q=${city}&appid=${APPID}`;
-
-  // 画面更新
-  const disp = (cityName, temp, humidity, pressure, description) => {
-    p_city.textContent = cityName;
-    p_temp.textContent = temp;
-    p_humidity.textContent = humidity;
-    p_pressure.textContent = pressure;
-    p_description.textContent = description;
-  };
 
   // fetchを実行する関数
   const update = (cityKey) => {
@@ -27,12 +11,11 @@ const main = () => {
     fetch(url)
       .then((response) => response.json())
       .then((json) => {
-        const cityName = json['name'];
-        const temp = json['main']['temp'] + ' ℃';
-        const humidity = json['main']['humidity'] + ' %';
-        const pressure = json['main']['pressure'] + ' hPa';
-        const description = json['weather'][0]['description'];
-        disp(cityName, temp, humidity, pressure, description);
+        p_city.textContent = json['name'];
+        p_temp.textContent = json['main']['temp'] + ' ℃';
+        p_humidity.textContent = json['main']['humidity'] + ' %';
+        p_pressure.textContent = json['main']['pressure'] + ' hPa';
+        p_description.textContent = json['weather'][0]['description'];
       });
   };
 
