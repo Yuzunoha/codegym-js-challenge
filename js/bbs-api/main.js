e7b3a9b2ec80f9cfbceae7121fdcc1cc27b7529f
EC2 Default User <ec2-user@ip-172-16-2-13.ap-northeast-1.compute.internal>
2022-08-28 16:11:01 +0000

    値が取れた

--- js/bbs-api/main.js
+++ js/bbs-api/main.js
@@ -4,13 +4,10 @@ const p = console.log;
 const host = 'http://localhost'; //ローカルで掲示板を動かす場合の設定
 
 //以下にコードを書きましょう。
-/*
-        <td><input type="text" id="registerName" placeholder="name" /></td>
-        <td><input type="text" id="registerBio" placeholder="bio" /></td>
-        <td><input type="text" id="registerPassword" placeholder="password" /></td>
-        <td><button id="registerSubmit">送信</button></td>
-*/
 const registerSubmit = document.getElementById('registerSubmit');
 registerSubmit.addEventListener('click', () => {
-  alert('押された');
+  const name = document.getElementById('registerName').value;
+  const bio = document.getElementById('registerBio').value;
+  const password = document.getElementById('registerPassword').value;
+  p({ name, bio, password });
 });
